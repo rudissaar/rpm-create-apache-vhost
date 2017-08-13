71aef2b6edd61bc25bf2a92a66b2c697f78ca86f
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2017-08-13 13:06:59 +0100

    Formatted code + Added basic README.

---
+++ README.md
@@ -0,0 +1,48 @@
+# RPM Create Apache VHOST Script
+
+### Script is designed to work RPM based operating systems.
+
+Bash script that makes adding new virtual hosts for Apache web server easy.
+Script can me moved to any directory, it doesn't use any relative dependencies and configuration files, but you still can configure script by declaring specific environment variables like:
+
+---
+`${APACHE_SERVER_ROOT}`
+
+Location of server's root, directory that contain server's configuration files.
+
+Default value:
+`'/etc/httpd'`
+
+---
+`${APACHE_DOCUMENT_ROOT}`
+
+Location on disk where document roots for new virtual hosts will be created.
+
+Default value:
+`'/var/www'`
+
+---
+`${APACHE_SUB_DOMAIN}`
+
+Name of the domain that will be inserted between name of the host and root level domain.
+
+Default value:
+`''`
+
+---
+
+### Dependencies:
+* bash
+* httpd
+
+## Author
+* Author: Kevin Rudissaar
+* Email: kevin.rudissaar[at]gmail.com
+* Twitter: [@KevinRudissaar](https://twitter.com/KevinRudissaar)
+
+## Donate
+If you found this script really useful or if you would just like to donate to developer, you can use following donation methods:
+
+[![Donate via PayPal](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EGU5RTCF69G82)
+
+BTC: 1HZNkcukFHYF5FKPTvsTnNQNiGQTHZcbR7
--- create-apache-vhost.sh
+++ create-apache-vhost.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
 if [[ ${#} != 1 ]]; then
-  echo '> You need to include at least 1 argument(name)'
-  echo
-  printf '%-12s' '> Usage: '
-  echo "$(basename ${0}) [name]"
-  printf '%-12s' '> Example: '
-  echo "$(basename ${0}) wordpress"
-  printf '%-12s' '> Example: '
-  echo "$(basename ${0}) magento"
-  printf '%-12s' '> Example: '
-  echo "$(basename ${0}) fox"
-  echo
-  exit 1
+    echo '> You need to include at least 1 argument(name)'
+    echo
+    printf '%-12s' '> Usage: '
+    echo "$(basename ${0}) [name]"
+    printf '%-12s' '> Example: '
+    echo "$(basename ${0}) wordpress"
+    printf '%-12s' '> Example: '
+    echo "$(basename ${0}) magento"
+    printf '%-12s' '> Example: '
+    echo "$(basename ${0}) fox"
+    echo
+    exit 1
 fi
 
 if [[ -n ${APACHE_SERVER_ROOT} ]]; then
-  SERVER_ROOT=${APACHE_SERVER_ROOT}
+    SERVER_ROOT=${APACHE_SERVER_ROOT}
 else
-  SERVER_ROOT='/etc/httpd'
+    SERVER_ROOT='/etc/httpd'
 fi
 
 if [[ -n ${APACHE_DOCUMENT_ROOT} ]]; then
-  DOCUMENT_ROOT=${APACHE_DOCUMENT_ROOT}
+    DOCUMENT_ROOT=${APACHE_DOCUMENT_ROOT}
 else
-  DOCUMENT_ROOT='/var/www'
+    DOCUMENT_ROOT='/var/www'
 fi
 
 if [[ -n ${APACHE_SUB_DOMAIN} ]]; then
-  SUB_DOMAIN=${APACHE_SUB_DOMAIN}
+    SUB_DOMAIN=${APACHE_SUB_DOMAIN}
 else
-  SUB_DOMAIN=''
+    SUB_DOMAIN=''
 fi
 
 VHOST_NAME=${1}
@@ -40,25 +40,27 @@ VHOST_NUM=$(printf "%03d" ${VHOST_NUM})
 CONF_NAME="${VHOST_NUM}-${VHOST_NAME}.conf"
 
 VHOST_HOST="${VHOST_NAME}"
+
 if [[ "${SUB_DOMAIN}" != '' ]]; then
-  VHOST_HOST="${VHOST_HOST}.${SUB_DOMAIN}"
+    VHOST_HOST="${VHOST_HOST}.${SUB_DOMAIN}"
 fi
+
 VHOST_HOST="${VHOST_HOST}.dev"
 
 if [[ ! -d "${DOCUMENT_ROOT}/${VHOST_NAME}" ]]; then
-  echo '> Creating document root for virtual host.'
-  mkdir "${DOCUMENT_ROOT}/${VHOST_NAME}"
+    echo '> Creating document root for virtual host.'
+    mkdir "${DOCUMENT_ROOT}/${VHOST_NAME}"
 fi
 
 echo '> Creating virtual host:' ${CONF_NAME}
 cat > "${SERVER_ROOT}/sites-available/${CONF_NAME}" <<EOL
 <VirtualHost _default_:80>
-  ServerName ${VHOST_HOST}
-  DocumentRoot ${DOCUMENT_ROOT}/${VHOST_NAME}
+    ServerName ${VHOST_HOST}
+    DocumentRoot ${DOCUMENT_ROOT}/${VHOST_NAME}
 
-  ErrorLog /var/log/httpd/${VHOST_NAME}-error.log
-  LogFormat "%h %t \"%r\" %>s \"%{User-agent}i\"" extended
-  CustomLog /var/log/httpd/${VHOST_NAME}-access.log extended
+    ErrorLog /var/log/httpd/${VHOST_NAME}-error.log
+    LogFormat "%h %t \"%r\" %>s \"%{User-agent}i\"" extended
+    CustomLog /var/log/httpd/${VHOST_NAME}-access.log extended
 </VirtualHost>
 EOL
 
@@ -67,8 +69,8 @@ ln -sf "${SERVER_ROOT}/sites-available/${CONF_NAME}" "${SERVER_ROOT}/sites-enabl
 
 grep -e ${VHOST_HOST} /etc/hosts
 if [[ ${?} -ne 0 ]]; then
-  echo '> Adding hostname entry for virtual host.'
-  cat >> /etc/hosts <<EOL
+    echo '> Adding hostname entry for virtual host.'
+    cat >> /etc/hosts <<EOL
 
 127.0.0.1    ${VHOST_HOST}
 ::1          ${VHOST_HOST}
